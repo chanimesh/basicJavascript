982c864ccf278004e32ef9be07bc6983c11ea1b9
Animesh <animeshc@thoughtworks.com>
2018-04-18 08:23:54 +0530

    array examples

---
+++ notes/Arrays/Arrays.js
@@ -0,0 +1,15 @@
+array1 = [1,2,3,4,5,6];
+
+
+array2 = Array.of(5,6,7,8,9);
+
+isArray = Array.isArray(array2);
+
+arraylength = array2.length;
+
+y = 0;
+
+array1.forEach((x) => y = x+y);
+
+array3 = array1.map((x) =>  x+3 );
+
