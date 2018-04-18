4f63ccd31d3a98801f297a7a1a35de7cf584b3bf
Animesh <animeshc@thoughtworks.com>
2018-04-18 07:38:32 +0530

    basic caliculator with an array input

---
+++ basicCaliculatorWithMultipleNumbers/basicCaliculatorWithMultipleNumbers.js
@@ -0,0 +1,26 @@
+function isNumeric(value) {
+ if(typeof(value) =="number") {
+   return true;
+ }
+ return false;
+}
+
+function calculator(numbers, operation) {
+if(!Array.isArray(numbers) || !numbers.every(isNumeric))
+{
+   return "please check your input Array";
+}
+
+ switch(operation){
+   case "+":
+     return  numbers.reduce(function totalSum(sum,value) { return sum + value });
+   case "-":
+     return  numbers.reduce(function totalDifference(difference, value){ return difference - value })
+   case "/":
+     return "we don't support division yet";
+   case "X":
+     return  numbers.reduce(function totalProduct(product,value) { return product * value });
+   default:
+     return "please check your input operation";
+ }
+}
