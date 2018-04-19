6a3efa323037ca88ebedb9a70eec9c1cda8c1f29
Animesh <animeshc@thoughtworks.com>
2018-04-19 08:23:44 +0530

    WIP strings and basic caliculator with string input

---
+++ basicCalculatorWIthStringAsInput/basicCalculatorWithStringAsInput.js
@@ -0,0 +1,4 @@
+function basicCalculatorWithStringAsInput(string) {
+   operations = ["+","-","*","/"];
+
+}
---
+++ notes/Strings/Strings.js
@@ -0,0 +1,10 @@
+string1 = "2+3"
+string2 = new String('2+3')
+eval_result1= eval(string1);
+eval_result2= eval(string2);
+
+
+string3 = "This is a string and used for testing strings"
+
+string3.charAt(5)
+string3.charCodeAt(4)
