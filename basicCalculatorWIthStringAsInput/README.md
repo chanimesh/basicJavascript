98822424604e65ec3599241ce92a0446588f2056
Animesh <animeshc@thoughtworks.com>
2018-04-27 08:32:46 +0530

    basic caliculator with string input working for few scenarios, should handle other cases mentioned in readme

--- basicCalculatorWIthStringAsInput/README.md
+++ basicCalculatorWIthStringAsInput/README.md
@@ -9,4 +9,13 @@ it should the do the following operations
     + / for division
     + and also brackets ()
     
-        
+
+
+
+**Happy paths**
+
+given a string with brackets it shoud return result
+
+error scenario:
+negative result in a expression of brackets will lead to not matching string for operations,
+like 8 x -4 from (2 + 3) x (4 - 8) doesn't match.
--- basicCalculatorWIthStringAsInput/basicCalculatorWithStringAsInput.js
+++ basicCalculatorWIthStringAsInput/basicCalculatorWithStringAsInput.js
@@ -4,7 +4,7 @@ function performOperation(string1,string2,operation) {
 
     val1 = parseFloat(string1.match(/\d[\.\d]*$/));
     val2 = parseFloat(string2.match(/^\d[\.\d]*/));
-    
+
 // TODO: need to check of characters other than digits and operations
     let newString1 = string1.replace(/\d[\.\d]*$/,'');
     let newString2 = string2.replace(/^\d[\.\d]*/,'');
@@ -33,16 +33,21 @@ function performOperation(string1,string2,operation) {
 function basicCalculatorWithStringAsInput(string) {
     //TODO: need to handle brackets
    const operations = {
-       product: 'x',
        division: '/',
+       product: 'x',
        sum: '+',
        difference: '-',
    };
 
-   let result = Object.values(operations)
+   let operationsResult = Object.values(operations)
        .reduce((object_full, current_operation) => {
 
        let newString = object_full.split(current_operation);
+
+       if (newString.length == 1) {
+           return object_full;
+       }
+
        object_full = newString.reduce(
            (accumilator, current_value, current_index, thisArray) => {
            if (thisArray.length > current_index + 1)
@@ -54,13 +59,35 @@ function basicCalculatorWithStringAsInput(string) {
        return object_full;
        },
            string);
+   return operationsResult;
 
+}
+
+function checkForBrackets(string)
+{
+    let fullArray = string.match(/\([\d.\d\s+\-\/x]*\)/g);
+
+    let newArray = fullArray.map(
+        (current_value) => {
+            return {
+                expression : current_value,
+                value : basicCalculatorWithStringAsInput(current_value.slice(1,-1)),
+            }
+        }
+    );
+    let bracketsReplaced = newArray.reduce(
+        (accumilater, current_value) =>
+           accumilater = accumilater.replace(current_value.expression,current_value.value)
+    , string);
+    if(bracketsReplaced.match(/\([\d.\d\s+\-\/x]*\)/g))
+        bracketsReplaced = checkForBrackets(bracketsReplaced);
+    return bracketsReplaced;
 }
 
 
 
-let testString = ' 2 + 3 x 4 - 8 / 10 '
+let testString = ' ((2 + 3) x (8 - 4) / 10) ';
 
-let result = basicCalculatorWithStringAsInput(testString);
+let fullResult = checkForBrackets(testString);
 
-console.log(result);
+console.log(fullResult);
